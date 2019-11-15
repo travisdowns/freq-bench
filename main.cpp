251989fa60baf16158f3420211e1dddc5c8f9e16
tdowns <travis.downs@gmail.com>
2019-11-15 16:20:15 -0500

    wip

--- main.cpp
+++ main.cpp
@@ -798,8 +798,9 @@ void runOne(const test_description* test,
     hot_wait(1000000);
     auto args = bargs.get_args();
 
-    constexpr size_t test_cycles = 100 * 1000ull * 1000ull;
-    constexpr size_t resolution_cycles = 1000ull * 1000ull; // cycles
+    constexpr size_t test_cycles       = 100 * 1000ull * 1000ull;
+    constexpr size_t period_cycles     =  10 * 1000ull * 1000ull;
+    constexpr size_t resolution_cycles =          10ull * 1000ull; // cycles
     for (size_t repeat = 0; repeat < bargs.repeat_count; repeat++) {
         _mm256_zeroupper();
 
@@ -809,21 +810,24 @@ void runOne(const test_description* test,
 
         uint64_t test_deadline = rdtsc() + test_cycles;
 
-        uint64_t tsc = rdtsc(), start_tsc = tsc, sample_deadline = tsc;
+        uint64_t tsc = rdtsc(), start_tsc = tsc, sample_deadline = tsc, period_deadline = tsc;
         Stamp prior_stamp = config.stamp();
         while (tsc < test_deadline) {
             // execute the payload instruction
             test->call_f(args);
 
-            sample_deadline += resolution_cycles;
-            while ((tsc = rdtsc()) < sample_deadline) {
-                // busy wait
-            }
+            period_deadline += period_cycles;
+            while (tsc < period_deadline) {
+                sample_deadline += resolution_cycles;
+                while ((tsc = rdtsc()) < sample_deadline) {
+                    // busy wait
+                }
 
-            Stamp stamp = config.stamp();
-            StampDelta delta = config.delta(prior_stamp, stamp);
-            results.push_back({tsc - start_tsc, delta});
-            prior_stamp = stamp;
+                Stamp stamp = config.stamp();
+                StampDelta delta = config.delta(prior_stamp, stamp);
+                results.push_back({tsc - start_tsc, delta});
+                prior_stamp = stamp;
+            }
         }
     }
 
