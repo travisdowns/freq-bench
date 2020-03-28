fadc4c098a4bb1c48424aafe7d88d1124571e910
Travis Downs <travis.downs@gmail.com>
2020-03-27 22:54:13 -0300

    plots.sh looking for wrong filename

--- scripts/plots.sh
+++ scripts/plots.sh
@@ -84,7 +84,7 @@ plot "$PREFIX-vporzmm_vz100-{0..2}.csv" "fig-ipc-zoomed-zmm" "Frequency (GHz)" "
     { "xy"  : [15020, 0], "width" :80, "height" : 4,"color" : "darkturquoise"} ]'
 
 # 8 us version of zmm lat
-plot "$PREFIX-vporzmm_vz100-10us-{0..2}.csv" "fig-ipc-zoomed-zmm-8us" "Frequency (GHz)" "Time (us)" "512-bit VPOR Transition (8 us sampling)" \
+plot "$PREFIX-vporzmm_vz100-8us-{0..2}.csv" "fig-ipc-zoomed-zmm-8us" "Frequency (GHz)" "Time (us)" "512-bit VPOR Transition (8 us sampling)" \
     $xcols_arg $ycols_arg --cols2-by-name "IPC" --ylim 2.7 3.3 --xlim 14950 15150 --ylabel2 IPC --ylim2 0 1.2 \
     --marker=. --marker2=. --legend-loc='upper right'
 
